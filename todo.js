1af80143ce4e8c50a709ecd3b985da9a8ca865f3
Kyle Devine <devinenoise@gmail.com>
2021-08-14 16:07:30 -0700

    working with dev tools

--- todo.js
+++ todo.js
@@ -4,19 +4,19 @@ let todos = ['Item 1', 'Item 2', 'Item 3'];
 // display
 console.log(`Your To Do list contains`, todos);
 
-// add
+// add a todo
 const addTodo = todo => {
   todos.push(todo);
   console.log(todos);
 };
 
-// edit
+// edit a todo
 const editTodo = (index, newTodo) => {
   todos[index] = newTodo;
   console.log(todos);
 };
 
-// delete
+// delete todo
 const deleteTodo = index => {
   todos.splice(index, 1);
   console.log(todos);
